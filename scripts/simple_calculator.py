d9a9d4561de62102ff3ff2947f71e4c9ac5b5aa1
GitOutLaw <58158920+webverto@users.noreply.github.com>
2020-09-28 22:54:53 -0400

    Updates

--- scripts/simple_calculator.py
+++ scripts/simple_calculator.py
@@ -1,17 +1,22 @@
-print('Simple Calculator | version: 0.001')
+print()
+print('Simple Calculator | Version: 0.03')
 print('Addition: + | Subtract: - | Multiply: * | Divide: / ')
 print()
 
+
 def operation():
     while True:
-        calculation = input('Please enter the calculation: ')
-        result = eval(calculation)
+        user_input = input('Please enter a calculation: ')
+        result = eval(user_input)
         end = ''
-        print('The result of your entered calculations is:', result)
-        if (calculation == 'end'):
-            print('<--- Program End --->')
+        print('The result of your entered calculations is: ', result)
+        print('Enter exit to end the program')
+        if user_input == 'end':
+            print('End Program')
             break
         else:
             continue
+        
+
 operation()
 
--- scripts/tempCodeRunnerFile.py
+++ scripts/tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-    'period': period_names,
-        'short_descriptions': short_descriptions,
-        'temperatures': temperatures, 
+end = ''
