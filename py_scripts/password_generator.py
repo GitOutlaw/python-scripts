164710c3ad69f4ba681b830b084c18ea8461f7eb
GitOutLaw <58158920+webverto@users.noreply.github.com>
2020-10-05 15:47:51 -0400

    update

--- py_scripts/password_generator.py
+++ py_scripts/password_generator.py
@@ -2,9 +2,18 @@ import string
 import secrets
 
 
-def password_generator():
-    alphabet = string.ascii_letters + string.digits
-    password = ''.join(secrets.choice(alphabet) for i in range(12))
-    print(password)
-    
-    
+def password_generator(): 
+        print('Password Generator')
+        try:
+            characters = int(input('How many characters to: '))
+
+        except ValueError:
+            print("Please enter a number")            
+            # continue
+        else:
+            alphabet = string.ascii_letters + string.digits
+            password = ''.join(secrets.choice(alphabet) for i in range(characters))
+            print(password)
+           
+            
+password_generator()  
