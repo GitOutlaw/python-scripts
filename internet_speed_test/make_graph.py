f4097a23d5a123706431676e585444a20b148583
GitOutLaw <58158920+webverto@users.noreply.github.com>
2020-10-25 18:22:19 -0400

    update

--- internet_speed_test/make_graph.py
+++ internet_speed_test/make_graph.py
@@ -16,3 +16,13 @@ with open('D:/dev/gits/Python-Scripts/internet_speed_test/data/internet_test.csv
         upload.append(float(row[2]))
 
 print(times, "\n", download, "\n", upload)
+
+
+# plt.figure(30, 30)
+# plt.plot(time, download, label='download', color='r')
+# plt.plot(time, upload, label='upload', color='b')
+# plt.xlabel('time')
+# plt.ylabel('speed in Mb/s')
+# plt.title("internet speed")
+# plt.legend()
+# plt.savefig('test_graph.jpg', bbox_inches='tight')
