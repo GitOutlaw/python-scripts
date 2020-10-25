d14c473eb9a687f4443708771eb0d4d2d610f159
GitOutLaw <58158920+webverto@users.noreply.github.com>
2020-10-25 01:18:13 -0400

    new file

---
+++ game_actions.py
@@ -0,0 +1,39 @@
+import random
+
+
+class Wizard:
+
+    def __init__(self, name, power, damage):
+        self.name = name
+        self.power = power
+        self.damage = damage
+
+    def attack(self):
+        print(f'{self.name} uses {self.power} for an attack of {self.damage}')
+
+    def heal(self):
+        pass
+
+
+class Ranger:
+
+    def __init__(self, name, power,):
+        self.name = name
+        self.power = power
+        # self.damage = damage
+
+    def attack_pierce(self):
+        min_value = 38
+        max_value = 100
+        # print(random.randint(min_value, max_value))
+        print(
+            f'{self.name} uses {self.power} for an attack of {random.randint(min_value, max_value)}.')
+
+    def heal(self):
+        pass
+
+
+ranger1 = Ranger('Max', 'Rain of Arrows')
+# ranger1 = Ranger('Argorn', 'Rain of Arrows', 98)
+
+ranger1.attack_pierce()
