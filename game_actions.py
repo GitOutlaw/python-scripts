34fbbc9eb0a9e8c8959c24eee8c93c3ba4c8ce4d
GitOutLaw <58158920+webverto@users.noreply.github.com>
2020-10-25 01:32:53 -0400

    Update game_actions.py

--- game_actions.py
+++ game_actions.py
@@ -6,10 +6,15 @@ class Wizard:
     def __init__(self, name, power):
         self.name = name
         self.power = power
-        self.damage = damage
 
     def attack_lighting(self):
-        min_value = 38
+        min_value = 47
+        max_value = 100
+        print(
+            f'{self.name} uses {self.power} for an attack of {random.randint(min_value, max_value)}.')
+        
+    def attack_fire_ball(self):
+        min_value = 47
         max_value = 100
         print(
             f'{self.name} uses {self.power} for an attack of {random.randint(min_value, max_value)}.')
@@ -26,17 +31,24 @@ class Ranger:
         # self.damage = damage
 
     def attack_pierce(self):
-        min_value = 38
+        min_value = 60
         max_value = 100
         # print(random.randint(min_value, max_value))
         print(
-            f'{self.name} uses {self.power} for an attack of {random.randint(min_value, max_value)}.')
+            f'{self.name} uses {self.power} for an attack of <{random.randint(min_value, max_value)}>.')
 
     def heal(self):
         pass
 
 
-ranger1 = Ranger('Max', 'Rain of Arrows')
-# ranger1 = Ranger('Argorn', 'Rain of Arrows', 98)
+ranger1 = Ranger('Max', 'Pierce')
+wizard1 = Wizard() 
 
 ranger1.attack_pierce()
+
+wizard1.attack_fire_ball()
+
+
+
+
+
