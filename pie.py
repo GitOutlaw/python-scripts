152916440f2e4530f23f458d0df1ea9387191fb4
GitOutLaw <58158920+GitOutlaw@users.noreply.github.com>
2021-04-20 19:02:07 -0400

    New App
    File Back up script v1

--- pie.py
+++ pie.py
@@ -6,14 +6,13 @@ class Cat():
         self.age = age
 
 
-
 cat1 = Cat('Butters', 12)
 cat2 = Cat('Kitty', 8)
 cat3 = Cat('Misty', 9)
+cat4 = Cat('Whiskers', 15)
 
 def find_oldest_cat(*args):
     return max(args)
 
-print(f'The oldest cat is {find_oldest_cat(cat1.age, cat2.age, cat3.age)}')
 
-
+print(f'The oldest cat is {find_oldest_cat(cat1.age, cat2.age, cat3.age, cat4.age)}')
