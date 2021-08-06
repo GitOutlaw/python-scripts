a7effef0a8ab92d6cca5067849379b31146d2a9a
GitOutlaw <lrt29net@gmail.com>
2021-08-06 00:13:06 -0400

    Updates
    Updated FizzBuzz solutions

--- In Dev/Games/game_actions.py
+++ In Dev/Games/game_actions.py
@@ -1,5 +1,25 @@
 import random
 
+# character roles
+
+
+class Warrior:
+
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack_cleave(self):
+        min_value = 55
+        max_value = 100
+        print(
+            f'{self.name} uses {self.power} for an attack of {random.randint(min_value, max_value)} .')
+
+    def attack_rend(self):
+        min_value = 35
+        max_value = 100
+        print(f'{self.name} uses {self.power} for an attack of <{random.randint(min_value, max_value)}> .')
+
 
 class Wizard:
 
@@ -10,14 +30,12 @@ class Wizard:
     def attack_lighting(self):
         min_value = 47
         max_value = 100
-        print(
-            f'{self.name} uses {self.power} for an attack of {random.randint(min_value, max_value)}.')
-        
+        print(f'{self.name} uses {self.power} for an attack of <{random.randint(min_value, max_value)}> .')
+
     def attack_fire_ball(self):
         min_value = 47
         max_value = 100
-        print(
-            f'{self.name} uses {self.power} for an attack of {random.randint(min_value, max_value)}.')
+        print(f'{self.name} uses {self.power} for an attack of <{random.randint(min_value, max_value)}> .')
 
     def heal(self):
         pass
@@ -28,38 +46,28 @@ class Ranger:
     def __init__(self, name, power):
         self.name = name
         self.power = power
-        # self.damage = damage
 
     def attack_pierce(self):
         min_value = 60
         max_value = 100
         # print(random.randint(min_value, max_value))
-        print(
-            f'{self.name} uses {self.power} for an attack of <{random.randint(min_value, max_value)}>.')
+        print(f'{self.name} uses {self.power} for an attack of <{random.randint(min_value, max_value)}>.')
 
-    
     def attack_long_shot(self):
         min_value = 45
         max_value = 100
         # print(random.randint(min_value, max_value))
-        print(
-            f'{self.name} uses {self.power} for an attack of <{random.randint(min_value, max_value)}>.')
-
-    
-
+        print(f'{self.name} uses {self.power} for an attack of <{random.randint(min_value, max_value)}>.')
 
     def heal(self):
         pass
 
 
 ranger1 = Ranger('Max', 'Pierce')
-wizard1 = Wizard('Merlin', 'L')
+wizard1 = Wizard('Merlin', 'Lighting')
+warrior1 = Warrior('Conan', 'Rend')
+
 
 ranger1.attack_pierce()
-
 wizard1.attack_fire_ball()
-
-
-
-
-
+warrior1.attack_rend()
--- Snips/interview_questions/fizz_buzz.py
+++ Snips/interview_questions/fizz_buzz.py
@@ -4,12 +4,22 @@ instead of the number and for the multiples of five print 'Buzz'.
 For numbers which are multiples of both three and five print 'FizzBuzz'.
 """
 
+# standard solution
+
 for num in range(1,20):
-    string = ""
-    if num % 3 == 0:
-        string += "Fizz"
-    if num % 5 == 0:
-        string += "Buzz"
-    if num % 5 != 0 and num % 3 != 0:
-        string += str(num)
-    print(string)
+  string = ""
+  if num % 3 == 0:
+    string += 'Fizz'
+  if num % 5 == 0:
+    string += 'Buzz'
+  if num % 3 != 0 and num % 5 != 0:
+    string += str(num)
+  print(string)
+
+# lambda solution
+
+print(*map(lambda i: 'Fizz'*(not i%3)+'Buzz'*(not i%5) or i, range(1,20)),sep='\n')
+    
+  
+
+
