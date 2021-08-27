844098a88ae8b89875381538a3167c7d4f703fe5
GitOutlaw <lrt29net@gmail.com>
2021-08-27 19:45:52 -0400

    Updates
    Renamed hangman file. Renamed several variables.  Added length function to variable lives

--- in-dev/games/hangman.py
+++ in-dev/games/hangman.py
@@ -1,25 +1,26 @@
-from replit import clear
 import random
-from hangman_words import word_list
 from hangman_art import logo, stages
+from hangman_words import word_list
+from replit import clear
 
+print(logo)
+
+game_is_finished = False
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
-end_of_game = False
-lives = 6
-
-print(logo)
+lives = len(stages) - 1
 
 # Testing code
 # print(f'Pssst, the solution is {chosen_word}.')
 
 # Create blanks
+
 display = []
 for _ in range(word_length):
     display += "_"
 
-while not end_of_game:
+while not game_is_finished:
     guess = input("Guess a letter: ").lower()
 
     clear()
@@ -29,26 +30,22 @@ while not end_of_game:
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-        elif guess in guess:
-            print(f"You've already guessed {guess}")
+    print(f"{' '.join(display)}")
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     # Check if user is wrong.
     if guess not in chosen_word:
-
-        print(
-            f"You guessed {guess}, that's not in the word. You lose a life")
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
         lives -= 1
-
         if lives == 0:
-            end_of_game = True
+            game_is_finished = True
             print("You lose.")
-
-    # Join all the elements in the list and turn it into a String.
-    print(f"{' '.join(display)}")
 
     # Check if user has got all letters.
     if "_" not in display:
-        end_of_game = True
+        game_is_finished = True
         print("You win.")
 
     print(stages[lives])
