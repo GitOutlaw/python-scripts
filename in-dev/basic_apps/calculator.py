8305a94ebd54858be606fb97fc60caf9166e3046
GitOutlaw <lrt29net@gmail.com>
2021-09-09 11:24:02 -0400

    Update
    Add basic calculator.

---
+++ in-dev/basic_apps/calculator.py
@@ -0,0 +1,67 @@
+# Calculator
+from calculator_art import logo
+
+
+def add(n1, n2):  # Add
+    return n1 + n2
+
+
+def subtract(n1, n2):  # Subtract
+    return n1 - n2
+
+
+def multiply(n1, n2):  # Multiply
+    return n1 * n2
+
+
+def divide(n1, n2):  # Divide
+    return n1 / n2
+
+
+def exponent(n1, n2):  # Divide
+    return n1 ** n2
+
+
+# Operation symbols dictionary keys are symbols and values are functions
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    "**": exponent,
+}
+
+
+def calculator():
+    """Recursion Function: Asks user to input first number, operation and second number.  If 'y' selected,
+    continue to add numbers and operations. If 'n' selected, program restarts and allows new input of
+    numbers and operations.
+    """
+    print(logo)
+
+    num1 = float(input("What's the first number?: "))
+
+    # Display operation symbols
+    for symbol in operations:
+        print(symbol)
+
+    should_continue = True
+
+    while should_continue:
+
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        # Continue or reset calculator
+        if input(f"Type 'y' to continue calculating with {answer} or 'n' to start a new calcultation: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
