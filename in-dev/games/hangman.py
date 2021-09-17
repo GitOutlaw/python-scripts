92954fcb3dfbb7fbe912d8ebcb5cd20c022f12e1
GitOutlaw <lrt29net@gmail.com>
2021-09-17 08:59:55 -0400

    Updated
    Updated functionality of hangman.

--- in-dev/games/hangman.py
+++ in-dev/games/hangman.py
@@ -4,6 +4,7 @@ from hangman_words import word_list
 from replit import clear
 
 print(logo)
+
 game_is_finished = False
 lives = len(stages) - 1
 
@@ -11,33 +12,31 @@ chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
 display = []
+
+# For loop takes word length and replaces and hides the letters.
 for _ in range(word_length):
     display += "_"
 
 while not game_is_finished:
     guess = input("Guess a letter: ").lower()
-
-    # Use the clear() function imported from replit to clear the output between guesses.
     clear()
-
     if guess in display:
         print(f"You've already guessed {guess}")
-
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
     print(f"{' '.join(display)}")
-
     if guess not in chosen_word:
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
             game_is_finished = True
             print("You lose.")
-
+            print(f"The word was {chosen_word}")
     if not "_" in display:
         game_is_finished = True
-        print("You win.")
+        print(f"Correct! {chosen_word}")
+        print("You win!")
 
     print(stages[lives])
