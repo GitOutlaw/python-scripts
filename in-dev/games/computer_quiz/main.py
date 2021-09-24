41517e8de2ced78bca69d98dad03d376cf2bb60a
GitOutlaw <lrt29net@gmail.com>
2021-09-24 14:53:42 -0400

    Updates
    Created new folders for game apps.
    
    Created a computer quiz game.

---
+++ in-dev/games/computer_quiz/main.py
@@ -0,0 +1,24 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from quiz_art import logo
+
+question_bank = []
+
+print(logo)
+
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+    
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
