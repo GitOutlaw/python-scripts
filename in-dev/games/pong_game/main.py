8a7044e9b136ea812e27dc6a4ae7316b97b1d5e2
John Crockett <58158920+GitOutlaw@users.noreply.github.com>
2021-10-13 14:48:04 -0400

    Update
    Created and added pong game using the Turtle module in python.

---
+++ in-dev/games/pong_game/main.py
@@ -0,0 +1,57 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import ScoreBoard
+import time
+
+
+# Setup the screen.
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+# Setup paddles ball and scoreboard.
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with the wall.
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle.
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect right paddle misses.
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect left paddle misses.
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+    # Endgame when a player reaches 10
+    if scoreboard.l_score == 3 or scoreboard.r_score == 3:
+        scoreboard.game_over()
+        game_is_on = False
+
+
+screen.exitonclick()
