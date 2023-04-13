ff680b8c4fd754f2dfe0432f08351484a9298820
John Crockett <58158920+GitOutlaw@users.noreply.github.com>
2023-04-13 12:46:32 -0400

    Updates

--- in-dev/basic_apps/random_word_generator.py
+++ in-dev/basic_apps/random_word_generator.py
@@ -8,7 +8,7 @@ r.get_random_words()
 # Return Word of the day
 r.word_of_the_day()
 
-# print(r.get_random_word(hasDictionaryDef="True", includePartOfSpeech="noun,verb", minCorpusCount=1, maxCorpusCount=10, minDictionaryCount=1, maxDictionaryCount=10, minLength=5, maxLength=10))
+print(r.get_random_word(hasDictionaryDef="True", includePartOfSpeech="noun,verb", minCorpusCount=1, maxCorpusCount=10, minDictionaryCount=1, maxDictionaryCount=10, minLength=5, maxLength=10))
 
 
 print(r.get_random_word(hasDictionaryDef='True', includePartOfSpeech="verb"))
